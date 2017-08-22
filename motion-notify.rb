d156d52232b75feb5f4e2b009cb4c8f1aca54eb3
Jeremy Stephens <jeremy.f.stephens@vanderbilt.edu>
2017-08-22 15:36:17 -0500

    Auto-daemonize with mutex via DRb

--- motion-notify.rb
+++ motion-notify.rb
@@ -1,6 +1,45 @@
 require 'getoptlong'
 require 'date'
-require 'google_drive'
+require 'drb'
+
+class Worker
+  def initialize(config_path, folder_id, unlink)
+    require 'google_drive'
+    @config_path = config_path
+    @folder_id = folder_id
+    @unlink = unlink
+    @mutex = Mutex.new
+  end
+
+  def upload(datetime, frame, picture_path)
+    @mutex.synchronize do
+      coll = parent
+
+      # find or create folder for date
+      date_title = datetime.strftime("%Y-%m-%d")
+      sub_coll = coll.subcollection_by_title(date_title)
+      if sub_coll.nil?
+        sub_coll = coll.create_subcollection(date_title)
+      end
+
+      # upload picture
+      sub_coll.upload_from_file(picture_path, "#{datetime.strftime("%H:%M:%S")}-#{frame}")
+      if @unlink
+        File.unlink(picture_path)
+      end
+    end
+  end
+
+  private
+
+  def session
+    @session ||= GoogleDrive::Session.from_config(@config_path)
+  end
+
+  def parent
+    @parent ||= session.file_by_id(@folder_id)
+  end
+end
 
 def print_usage(program_name)
     puts <<EOF
@@ -23,6 +62,9 @@ def print_usage(program_name)
 
 -p picture-path, --picture picture-path:
    Full path of picture
+
+-D drb-uri, --drb drb-uri:
+   URI of DRb process
 EOF
 end
 
@@ -33,6 +75,8 @@ opts = GetoptLong.new(
   [ '--datetime',   '-d',  GetoptLong::REQUIRED_ARGUMENT ],
   [ '--frame',      '-F',  GetoptLong::REQUIRED_ARGUMENT ],
   [ '--picture',    '-p',  GetoptLong::REQUIRED_ARGUMENT ],
+  [ '--drb',        '-D',  GetoptLong::REQUIRED_ARGUMENT ],
+  [ '--no-unlink',  '-U',  GetoptLong::NO_ARGUMENT ],
 )
 
 program_name = $0
@@ -41,6 +85,8 @@ config_path = nil
 datetime = nil
 frame = nil
 picture_path = nil
+unlink = true
+drb_uri = "druby://localhost:8787"
 
 opts.each do |opt, arg|
   case opt
@@ -57,6 +103,10 @@ opts.each do |opt, arg|
     frame = arg
   when '--picture'
     picture_path = arg
+  when '--drb'
+    drb_uri = arg
+  when '--no-unlink'
+    unlink = false
   end
 end
 
@@ -94,23 +144,22 @@ if !valid
   exit
 end
 
-session = GoogleDrive::Session.from_config(config_path)
-coll = session.file_by_id(folder_id)
-if !coll.is_a?(GoogleDrive::Collection)
-  puts "--folder-id does not point to a folder"
-  print_usage(program_name)
-  exit
-end
-
-# find or create folder for date
-date_title = datetime.strftime("%Y-%m-%d")
-sub_coll = coll.subcollection_by_title(date_title)
-if sub_coll.nil?
-  sub_coll = coll.create_subcollection(date_title)
-end
-
-# upload picture
+# try to connect to existing DRb process first
+server = DRbObject.new_with_uri(drb_uri)
+tried = false
 begin
-  sub_coll.upload_from_file(picture_path, "#{datetime.strftime("%H:%M:%S")}-#{frame}")
-  File.unlink(picture_path)
+  server.upload(datetime, frame, picture_path)
+rescue DRb::DRbConnError => exp
+  # failed, boot DRb
+  fork do
+    DRb.start_service(drb_uri, Worker.new(config_path, folder_id, unlink))
+    DRb.thread.join
+  end
+  sleep 5
+  if !tried
+    tried = true
+    retry
+  else
+    raise "Couldn't start server"
+  end
 end
